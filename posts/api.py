c80ff8dcafc3f40d864297af89e25f143f635026
Vadim Vzorov <vzblazer@gmail.com>
2017-04-19 00:30:58 +0200

    added post editing

--- posts/api.py
+++ posts/api.py
@@ -8,6 +8,14 @@ from . import decorators
 from posts import app
 from .database import session
 
+post_schema = {
+    "properties": {
+        "title" : {"type" : "string"},
+        "body": {"type": "string"}
+    },
+    "required": ["title", "body"]
+}
+
 @app.route("/api/posts", methods=["GET"])
 @decorators.accept("application/json")
 def posts_get():
@@ -67,3 +75,53 @@ def delete_post(id):
     message = "Deleted post with id {}".format(id)
     data = json.dumps({"message": message})
     return Response(data, 200, mimetype="application/json")
+
+@app.route("/api/posts", methods=["POST"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def posts_post():
+    """ Add a new post """
+    data = request.json
+
+    # Check that the JSON supplied is valid
+    # If not you return a 422 Unprocessable Entity
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+    # Add the post to the database
+    post = models.Post(title=data["title"], body=data["body"])
+    session.add(post)
+    session.commit()
+
+    # Return a 201 Created, containing the post as JSON and with the
+    # Location header set to the location of the post
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 201, headers=headers,
+                    mimetype="application/json")
+
+@app.route("/api/posts/<int:id>/edit", methods=["PUT"])
+@decorators.accept("application/json")
+@decorators.require("application/json")
+def post_edit(id):
+    """Edits existing post"""
+    data = request.json
+
+    try:
+        validate(data, post_schema)
+    except ValidationError as error:
+        data = {"message": error.message}
+        return Response(json.dumps(data), 422, mimetype="application/json")
+
+
+    post = session.query(models.Post).get(id)
+    post.title = data["title"]
+    post.body = data["body"]
+    session.commit()
+    data = json.dumps(post.as_dictionary())
+    headers = {"Location": url_for("post_get", id=post.id)}
+    return Response(data, 202, headers=headers,
+                    mimetype="application/json")
